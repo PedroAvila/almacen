9e81801028402b1930a867e1ab3d834f3e0ede2c
PedroAvila <minimainor_austin@hotmail.com>
2021-04-28 07:13:21 -0500

    Validando del lado del servidor

---
+++ Almacen.Web/Controllers/PersonaController.cs
@@ -0,0 +1,33 @@
+﻿using Almacen.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Almacen.Web.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(Persona datos)
+        {
+            ActionResult resultado;
+            if (ModelState.IsValid)
+            {
+                resultado = Content("Datos correctos");
+            }
+            else
+            {
+                resultado = View(datos);
+            }
+            return resultado;
+        }
+    }
+}
--- Almacen.Web/Models/Persona.cs
+++ Almacen.Web/Models/Persona.cs
@@ -10,17 +10,17 @@ namespace Almacen.Web.Models
     {
         public int ID { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Debe proporcionar un nombre")]
         public string Nombre { get; set; }
 
-        [StringLength(5, MinimumLength = 5)]
+        [StringLength(5, MinimumLength = 5, ErrorMessage = "El código postal debe ser de 5 caracteres")]
         public string CodigoPostal { get; set; }
 
-        [Range(0,100)]
+        [Range(0,100, ErrorMessage = "El porcentaje debe de ser de 0 a 100%")]
         public byte DominioIngles { get; set; }
 
-        [Required]
-        [RegularExpression(@".+\@.+\..+")]
+        [Required(ErrorMessage = "El correo es requerido")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "La dirección de correo debe ser válida")]
         public string Correo { get; set; }
     }
 }
